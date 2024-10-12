4ea0894c02ae1267c8ba5d890e21244b051658d2
Myungjin Ki <myungjinki91@gmail.com>
2024-10-12 16:34:42 +0900

    Modify auth public key

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
-export const ts = "1728718177774";
-export const apikey = "3ca3a441c94b9f110af4705b56f1e227";
-export const hash = "d4cc38450030130fb05b1667bd13ae84";
+export const auth = {
+  ts: "1728718177774",
+  apikey: "3ca3a441c94b9f110af4705b56f1e227",
+  hash: "d4cc38450030130fb05b1667bd13ae84",
+};
